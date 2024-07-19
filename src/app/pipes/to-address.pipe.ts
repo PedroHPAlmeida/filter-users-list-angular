e6d77f85bf616522102243554955454d8577e65e
Pedro H. P. Almeida <pedro.6571almeida@gmail.com>
2024-07-18 21:04:45 -0300

    refact: pipe toAddress

--- src/app/pipes/to-address.pipe.ts
+++ src/app/pipes/to-address.pipe.ts
@@ -6,6 +6,17 @@ import { Address } from '../types/user/address.type';
 })
 export class ToAddressPipe implements PipeTransform {
   transform(address: Address): string {
+    const INVALID_ADDRESS =
+      !address ||
+      !address.rua ||
+      !address.cidade ||
+      !address.estado ||
+      address.numero === undefined || address.numero === null;
+
+    if(INVALID_ADDRESS) {
+      return 'Endereço indisponível ou inválido';
+    }
+
     return `${address.rua}, ${address.numero}, ${address.cidade} - ${address.estado}`;
   }
 }
